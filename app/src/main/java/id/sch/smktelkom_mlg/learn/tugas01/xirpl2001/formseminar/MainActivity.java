441f1143b89fb4398f6c7078b4a72c2637133a8a
Aisyatush Shofiah <aisyatushshofiah@gmail.com>
2016-09-11 20:41:35 +0700

    Third commit ; Validasi nama & email, setting radio button, setting checkbox

--- app/src/main/java/id/sch/smktelkom_mlg/learn/tugas01/xirpl2001/formseminar/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/tugas01/xirpl2001/formseminar/MainActivity.java
@@ -4,26 +4,43 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    EditText etNama;
-    EditText etEmail;
-    EditText etPass;
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    EditText etNama, etEmail, etPass;
     Button bDaftar;
-    TextView tvHasil;
+    TextView tvHasil, tvHasil2, tvMapel;
+    RadioButton rbLK, rbPR;
+    CheckBox cbHTML, cbCSS, cbJS, cbPHP;
+    int nMapel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNama = (EditText) findViewById(R.id.etNama);
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        etPass = (EditText) findViewById(R.id.etPass);
-        bDaftar = (Button) findViewById(R.id.bDaftar);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
+        etPass = (EditText) findViewById(R.id.editTextPass);
+        bDaftar = (Button) findViewById(R.id.buttonDaftar);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil2 = (TextView) findViewById(R.id.textViewHasil2);
+        rbLK = (RadioButton) findViewById(R.id.radioButtonLk);
+        rbPR = (RadioButton) findViewById(R.id.radioButtonPr);
+        cbHTML = (CheckBox) findViewById(R.id.checkBoxHTML);
+        cbCSS = (CheckBox) findViewById(R.id.checkBoxCSS);
+        cbJS = (CheckBox) findViewById(R.id.checkBoxJS);
+        cbPHP = (CheckBox) findViewById(R.id.checkBoxPHP);
+        tvMapel = (TextView) findViewById(R.id.textViewMapel);
+        cbHTML.setOnCheckedChangeListener(this);
+        cbCSS.setOnCheckedChangeListener(this);
+        cbJS.setOnCheckedChangeListener(this);
+        cbPHP.setOnCheckedChangeListener(this);
 
         bDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,12 +52,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doProcess() {
+
         if (isValid()) {
             String nama = etNama.getText().toString();
             String email = etEmail.getText().toString();
-            String pass = etPass.getText().toString();
             tvHasil.setText("Atas nama " + nama + ", email Anda " + email);
         }
+
+        String gender = null;
+
+        if (rbLK.isChecked()) {
+            gender = rbLK.getText().toString();
+        } else if (rbPR.isChecked()) {
+            gender = rbPR.getText().toString();
+        }
+
+        if (gender == null) {
+            tvHasil2.setText("Anda belum memilih Gender");
+        } else {
+            tvHasil2.setText("Gender Anda ; " + gender);
+        }
+
+        String mapel = "Materi yang Anda ambil adalah ;\n";
+        int startlen = mapel.length();
+        if (cbHTML.isChecked()) mapel += cbHTML.getText() + "\\n";
+        if (cbCSS.isChecked()) mapel += cbCSS.getText() + "\\n";
+        if (cbJS.isChecked()) mapel += cbJS.getText() + "\\n";
+        if (cbPHP.isChecked()) mapel += cbPHP.getText() + "\\n";
+
+        if (mapel.length() == startlen) mapel += "Anda Belum memilih Materi";
+
+        tvHasil2.setText(mapel);
     }
 
     private boolean isValid() {
@@ -65,5 +107,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundbutton, boolean isChecked) {
+        if (isChecked) nMapel += 1;
+        else nMapel -= 1;
+
+        tvMapel.setText("Materi (" + nMapel + " terpilih)");
     }
 }
