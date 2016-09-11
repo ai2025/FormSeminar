7d327792e0aaac1ab478762be15385e1b4d629a7
Aisyatush Shofiah <aisyatushshofiah@gmail.com>
2016-09-11 16:22:22 +0700

    First commit ; layout

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/tugas01/xirpl2001/formseminar/MainActivity.java
@@ -0,0 +1,12 @@
+package id.sch.smktelkom_mlg.learn.tugas01.xirpl2001.formseminar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
